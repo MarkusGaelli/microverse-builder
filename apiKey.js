43eaa32cd9f278f32e959ab08cbd23b35d2e896d
Markus Gaelli <markus.gaelli@gmail.com>
2022-06-15 17:27:54 +0200

    Update apiKey.js
    host encoding

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 /* Copy this into a file named apiKey.js */
-const apiKey = '1eBC2qdrgpJKcz3z4L4nlcxJ0gvnblD2vGsNybbGs';
-const appId = 'com.gmail.markus.gaelli.microverse';
+const apiKey = '1s3Mt1hafpFCFMiF2g776ajAllDvKh4tL0BchdL3l';
+const appId = 'microverse-builder';
 export default {apiKey, appId};
