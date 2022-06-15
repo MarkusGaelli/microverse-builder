4f9867e0a2f880f5cf58fb2569f827bea1c637d0
Markus Gaelli <markus.gaelli@gmail.com>
2022-06-15 17:34:26 +0200

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 /* Copy this into a file named apiKey.js */
 const apiKey = '1s3Mt1hafpFCFMiF2g776ajAllDvKh4tL0BchdL3l';
-const appId = 'microverse-builder';
+const appId = 'demo';
 export default {apiKey, appId};
